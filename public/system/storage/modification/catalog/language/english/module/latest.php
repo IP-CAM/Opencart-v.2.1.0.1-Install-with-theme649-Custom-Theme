bc713b455beeeadfc441ad3b4d6068110c01bd6d
Marco Foganholo <marco@lojavirtual.digital>
2016-07-15 17:12:43 -0300

    Up Files

---
+++ public/system/storage/modification/catalog/language/english/module/latest.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title'] = 'Latest';
+
+// Text
+$_['text_tax']      = 'Ex Tax:';
+
+			$_['text_sale']      = 'Sale';
+			
---
+++ public/system/storage/modification/catalog/language/russian/module/latest.php
@@ -0,0 +1,10 @@
+<?php
+// Heading
+$_['heading_title'] = 'Новые поступления';
+
+// Text
+
+			$_['text_sale']      = 'Скидка';
+			
+$_['text_tax']      = 'Без НДС:';
+
